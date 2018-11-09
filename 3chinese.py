d2d96e7672cae93b7a9b3afce4ae5b2a067e7dac
xin <datadev@126.com>
2018-11-09 23:30:50 +0800

    Create 3chinese.py

---
+++ 3chinese.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# 2、繁体转简体opencc
+# https://bintray.com/package/files/byvoid/opencc/OpenCC
+# 下载opencc-1.0.1-win64.7z。
+#
+# opencc -i wiki_text.txt -o test.txt -c t2s.json
+# -i 输入
+# -o 输出
